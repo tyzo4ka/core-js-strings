ab38a36cdfa1d45e3d620e8a6531bf199a034462
Евгения Генералова <generalova_evgenia@mail.ru>
2023-11-20 22:23:17 +0600

    feat: add formatTime and reverseString functions

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -251,8 +251,10 @@ function endsWith(someString, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  return `${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}`;
 }
 
 /**
@@ -265,8 +267,8 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(someString) {
+  return [...someString].reverse().join('');
 }
 
 /**
